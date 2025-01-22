a73cfdb0c7ae9744be1e389668d06ed7e38b75a6
Henok <hdbeleive@gmail.com>
2025-01-22 15:06:11 +0300

    correction

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ function initializeFormValidation(){
 
 //initialize validation variables
     let isValid=true;
-    let message=[ ];
+    let messages=[ ];
 
     if (username.length<3){
         isValid=false;
-        message.push("User Name must be greater than 2 characters.") ;
+        messages.push("User Name must be greater than 2 characters.") ;
      }
 
      if (!email.includes ("@")|| !email.includes (".")) {
         isValid=false;
-        message.push("Please enter a valid email adress.")
+        messages.push("Please enter a valid email adress.")
      }
 
      if (password.length<8) {
         isValid=false;
-        message.push("Please enter a password atleast 8 characters long.")
+        messages.push("Please enter a password atleast 8 characters long.")
      }
 
      feedbackDiv.style.display="block";
@@ -36,7 +36,7 @@ function initializeFormValidation(){
         feedbackDiv.style.color="#28a745";
 
      }else{
-        feedbackDiv.innerHTML = message.join ("<br>");
+        feedbackDiv.innerHTML = messages.join ("<br>");
         feedbackDiv.style.color="#dc3545";
      }
 
